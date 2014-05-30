69409cf079f9fd8c84f48848ba738bf1b75ce535
Rubiromi <hiromi.deyoung@gmail.com>
2014-05-30 15:26:26 -0600

    numbers challenge is done

---
+++ numbers.rb
@@ -0,0 +1,34 @@
+puts "Lets do some math:"
+
+
+puts "25 + 30 / 6 is "
+puts 25 + 30 / 6
+
+
+puts "100 - 25 * 3 / 4 is "
+puts 100 - 25 * 3 / 4
+
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+puts 3 + 2 < 5 -7
+
+puts "Is 5 greater than -2?"
+
+puts 5 > -2
+
+
+puts "Is 5 less than -2?"
+puts 5 < -2
+
+
+number1 = 10
+number2 = 5
+number3 = 2
+
+number1.to_s
+
+puts number1 / number2 = number2
+
+puts number2 % number3
+
+puts number1 * number1 == number2 * number3 * number1
