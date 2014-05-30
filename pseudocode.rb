a89e762a2820cc5978ad64a66a62ce8518a6cfaf
Rubiromi <hiromi.deyoung@gmail.com>
2014-05-30 16:28:25 -0600

    pseudocode is done

---
+++ pseudocode.rb
@@ -0,0 +1,20 @@
+# set a class name as TicTacToe
+# initialize players names and moves
+# welcome message
+# get player1's name and set as X
+# get player2's name and set as O
+# create a gameboared
+# display an empty gameboared
+# player1 plays
+# display a gameboared with player1's move
+# check for a winner
+# player2 plays
+# Display a gameboared with player2's move
+# check for a winner
+#
+# repeat these part until 
+# we have a winner
+# or
+# we are tie
+# Display a winner or
+# say, "it's a tie"
